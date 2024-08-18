62bcf0245ad199c12ff3f5e52282e6a738974c75
Your Name <you@example.com>
2024-08-18 07:32:46 +0300

    2024.08.18.

--- src/app/history/api/route.ts
+++ src/app/history/api/route.ts
@@ -1,11 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import {EMethod, postData} from "../../utils";
-import {Category} from "@prisma/client";
 
-const _404 = "No category with ID found";
 
-export async function GET(request: Request) {
+export async function GET() {
     const histories: Array<any> = await prisma.history.findMany({
     });
     return NextResponse.json(histories);
